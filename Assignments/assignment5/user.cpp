8890cef04cc04b043e01e4259f240ff0374e1538
xinruizhao19 <24307140096@m.fudan.edu.cn>
2026-04-16 16:10:14 +0800

    assignment 5 finished

--- Assignments/assignment5/user.cpp
+++ Assignments/assignment5/user.cpp
@@ -11,6 +11,38 @@ User::User(const std::string& name)
 {
 }
 
+/** 
+  * Destructor 
+ */
+User::~User() {
+  delete[] _friends;
+}
+
+/**
+ * Copy Constructor
+ */
+User::User(const User& other) : _name(other._name), _capacity(other._capacity), _size(other._size) {
+  _friends = new std::string[_capacity];
+  std::copy(other._friends, other._friends + _size, _friends);
+}
+
+/**
+ * Copy Assignable
+ */
+User& 
+User::operator =(const User& user) {
+  if (this != &user) {
+    delete[] _friends;
+    _name = user._name;
+    _capacity = user._capacity;
+    _size = user._size;
+
+    _friends = new std::string[_capacity];
+    std::copy(user._friends, user._friends + _size, _friends);
+  }
+  return *this;
+}
+
 /**
  * Adds a friend to this User's list of friends.
  * @param name The name of the friend to add.
@@ -63,3 +95,28 @@ void User::set_friend(size_t index, const std::string& name)
  * STUDENT TODO:
  * The definitions for your custom operators and special member functions will go here!
  */
+std::ostream& 
+operator<<(std::ostream& os, const User& u) {
+  os << "User(name=" << u._name << ", friends=[";
+  for (size_t i = 0; i < u._size; i++) {
+    std::string f = u._friends[i];
+    os << f;
+    if (i != u._size - 1) {
+      os << ", ";
+    }
+  } 
+  os << "])";
+  return os;
+} 
+
+User&
+User::operator+=(User& rhs) {
+  this->add_friend(rhs.get_name());
+  rhs.add_friend(_name);
+  return *this;
+}
+
+bool 
+User::operator<(const User& rhs) const{
+  return this->_name < rhs.get_name();
+}
--- Assignments/assignment5/user.h
+++ Assignments/assignment5/user.h
@@ -10,15 +10,26 @@ class User
 {
 public:
   User(const std::string& name);
+  ~User();
+  User(const User& user);
+  User& operator=(const User& user);
+
+  User(User&& user) = delete;
+  User& operator=(User&& user) = delete;
+
   void add_friend(const std::string& name);
   std::string get_name() const;
   size_t size() const;
   void set_friend(size_t index, const std::string& name);
 
+  User& operator+=(User& rhs);
+  bool operator<(const User& rhs) const;
+
   /** 
    * STUDENT TODO:
    * Your custom operators and special member functions will go here!
    */
+  friend std::ostream& operator << (std::ostream&os, const User& u);
 
 private:
   std::string _name;
